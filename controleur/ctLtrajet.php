5ec221b62c2d3a72636dc8b477f01e4f1abf91ee
Mapess <81568782+Agnosthos@users.noreply.github.com>
2023-03-28 13:27:46 +0200

    Add files via upload

---
+++ controleur/ctLtrajet.php
@@ -0,0 +1,142 @@
+<?php require_once 'modele/modeletrajet.php'; 
+		require_once 'modele/modelevehicule.php'; 
+$action = $_GET['action']; 
+switch($action){
+		//Affiche liste : 
+		case 'affichetrajet':
+		{   $listetrajet = ModelTrajet::affichetrajet();
+			include('vues/trajets/accueiltrajet.php');
+		   break;
+		}
+		case 'ajoutvehicule':
+			{  include('vues/trajets/ajoutvehicule.php');
+			   break;
+			}
+			case 'ajouttrajet':
+				{   $lesvehicules = Modelvehicule::affichevehicule($_SESSION['id']);
+					include('vues/trajets/formtrajet.php');
+				   break;
+				}
+
+		case 'enregistrer': 
+		{
+			    
+			    $dep = htmlspecialchars($_POST['lieudep']);
+				$arr = htmlspecialchars($_POST['lieuar']);
+				$Nbreplace = htmlspecialchars($_POST['nombreplace']);
+				$date = htmlspecialchars($_POST['date']);
+				/*$immat = htmlspecialchars($_POST['nimmat']);*/
+				$vehicule = htmlspecialchars($_POST['choivehicule']);
+				$immat = htmlspecialchars($_POST['immat']);
+				$iduserv = htmlspecialchars($_SESSION['id']);
+
+				if(preg_match("/[A-Z]{2}[0-9]{3}[A-Z]{2}/", $immat))
+				{
+
+					ModelTrajet::ajout($dep, $arr, $Nbreplace, $date, $vehicule, $iduserv, $immat);
+				$lesvehicules = Modelvehicule::affichevehicule($_SESSION['id']);
+			    include('vues/trajets/formtrajet.php');
+                break;
+				}
+
+				else
+				{
+				echo "Immatriculation non conforme";
+				include('vues/trajets/formtrajet.php');
+                break;
+				}
+
+		}
+		case 'listetrajet':
+			{  include('vues/trajets/cherchetrajet.php');
+			   break;
+			}
+		case 'affichelistetrajet': 
+			{   $listetrajet = ModelTrajet::affichetrajet();
+				include('vues/trajets/listetrajet.php');
+				break;
+			}
+			case 'mesreservestrajets': 
+				{   $listetrajet = ModelTrajet::mestrajetsreserves($_SESSION['id']);
+					include('vues/trajets/listetrajet.php');
+					break;
+				}
+		case 'affichelisteadmin': 
+			{   $listetrajet = ModelTrajet::affichetrajet();
+					include('vues/trajets/listeadmintrajet.php');
+					break;
+			}
+		case 'affichetrajetchercher': 
+			{   $listetrajet = ModelTrajet::cherchetrajet($_POST['trajet']);
+					include('vues/trajets/listeadmintrajet.php');
+					break;
+			}
+		case 'mestrajets':
+			{   $mestrajets = ModelTrajet::affichemestrajets($_SESSION['id']);
+				include('vues/trajets/mestrajets.php');
+				break;
+			}
+		case 'supprimertrajet':
+				{   
+					ModelTrajet::supprimertrajet($_GET['supp']);
+					//$mestrajets = ModelTrajet::affichemestrajets($_SESSION['id']);
+					$listetrajet = ModelTrajet::affichetrajet();
+					include('vues/trajets/listetrajet.php');
+					break;
+				}
+		case 'modifiertrajet':
+				{   /*ModelTrajet::modifiertrajet($_GET['supp']);
+					$mestrajets = ModelTrajet::affichemestrajets($_SESSION['id']);*/
+	                $untrajet = ModelTrajet::afficheletrajet($_GET['modif'],$_GET['user']);
+					$_SESSION['user']= $_GET['user'];
+					$_SESSION['modif']= $_GET['modif'];
+					include('vues/trajets/modiftrajets.php');
+					break;
+				}
+		case 'enregistrermodif':
+				{   /*ModelTrajet::modifiertrajet($_GET['supp']);
+						$mestrajets = ModelTrajet::affichemestrajets($_SESSION['id']);*/
+						$mestrajets = ModelTrajet::modiftrajet($_POST['date'],$_POST['lieudep'],$_POST['lieuar'],$_POST['choivehicule'],$_POST['nombreplace'],$_POST['immatricu'],$_SESSION['modif'],$_SESSION['user']);
+						/*$_SESSION['modif']= $_GET['modif'];*/
+						$listetrajet = ModelTrajet::affichetrajet();
+						include('vues/trajets/listetrajet.php');
+						break;
+				}
+		case 'reservationcov':
+				{   $cherchetrajet = ModelTrajet::chercherreserves($_GET['iduser'],$_GET['idcov'] );
+					$idep = ModelTrajet::cherchedep($_GET['idcov']);
+					$dep = $idep['Lieudep'];
+					$arr = $idep['Lieuarr'];
+					ModelTrajet::reservertrajets($_GET['iduser'],$_GET['idcov'],$dep,$arr, $_GET['cond'],$_GET['statut']);
+					$listetrajet = ModelTrajet::mestrajetsreserves($_SESSION['id']);	
+					include('vues/trajets/listetrajet.php');
+					break;
+				}
+		case 'demande':
+				{    		$listedemandetrajet = ModelTrajet::demandetrajets($_SESSION['id']);
+							include('vues/trajets/listedemandetrajet.php');
+							break;
+				}
+		case 'affichelistepersonne':
+					{    	$listepersonne = ModelTrajet::affichepersonne();
+							include('vues/trajets/listepersonnes.php');
+							break;
+					}
+		case 'choixcond':
+				{   if ($_GET['choix']=="Accepter")
+					{		
+							ModelTrajet::nbreplaces( $_GET['place'],$_GET['cov']); 
+					} 		
+					ModelTrajet::choixtrajets($_GET['idreservation'], $_GET['choix']);
+					$listedemandetrajet = ModelTrajet::demandetrajets($_SESSION['id']);
+							include('vues/trajets/listedemandetrajet.php');
+							break;
+				}
+	case 'affichelistepassager': 
+		{
+					$mespassagers = ModelTrajet::affichelistepassager($_SESSION['id']);
+					include('vues/trajets/listepassagers.php');
+					break;
+		}
+		
+}?>
